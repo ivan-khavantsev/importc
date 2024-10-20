e018215901b9ebc50b532200818ea16899141bf6
Ivan Khavantsev <ivan.khavantsev@gmail.com>
2024-10-21 00:35:33 +0500

    commit

--- import.c
+++ import.c
@@ -4,11 +4,8 @@
 #include <dlfcn.h>
 
 int main() {
-    // Имя динамической библиотеки
-    const char *lib_name = "/usr/lib/libvos.so";  // Пример имени библиотеки
-
     // Открываем библиотеку
-    void *handle = dlopen(lib_name, RTLD_LAZY);
+    void *handle = dlopen("/usr/lib/libvos.so", RTLD_LAZY);
     if (!handle) {
         fprintf(stderr, "Ошибка: %s\n", dlerror());
         exit(EXIT_FAILURE);
@@ -18,8 +15,8 @@ int main() {
     dlerror(); 
 
     // Получаем указатель на функцию
-    typedef int (*my_function_type)(int crc, const uint8_t *buf, size_t len);
-    my_function_type my_function = (my_function_type) dlsym(handle, "crc_Calculate32");
+    typedef int (*dlFunctionType)(int crc, const uint8_t *buf, size_t len);
+    dlFunctionType dlFunction = (dlFunctionType) dlsym(handle, "crc_Calculate32");
 
     const char *dlsym_error = dlerror();
     if (dlsym_error) {
@@ -28,40 +25,24 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Пример имени файла для проверки
-    //const char *filename = "firmware.original";
-    //const char *filename = "firmware-fixed2.img";
-
-   
-	
-	
-	
-	// ------------------------------
-	
-	
+	// Файл, для которого хотим посчитать контрольную сумму
 	FILE *file = fopen("cramfs.img", "rb");
     if (file == NULL) {
         perror("Ошибка открытия файла");
         return 1;
     }
 
+    // Изначальное значение CRC
 	int crc = 0x00000000;
-	//int crc = 0x464C457F;
-	
-	uint8_t buffer[1024];
-	size_t bytesRead;
-	
-	// Чтение файла блоками
+
+    // Чтение файла блоками
+    uint8_t buffer[1024];
+    size_t bytesRead;
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        crc = my_function(crc, buffer, bytesRead);
+        crc = dlFunction(crc, buffer, bytesRead);
     }
 
-    // Закрытие файла
     fclose(file);
 	printf("CRC: %x\n", crc);
-   
-	
-	
-	
     return 0;
 }
